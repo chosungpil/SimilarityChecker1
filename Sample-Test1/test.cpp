be10c549a0382d7b0104359bea4d7740b87952d8
Sungpil.cho <chosungpil@gmail.com>
2023-06-02 13:46:04 +0900

    Apply revewfeedback - using fixture

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,22 +1,22 @@
 #include "pch.h"
 #include "../Project6/SimilarityChecker.cpp"
-TEST(SimilarityCheckerTest, PerfectMatch) {
+class SimilarityChecker_Fixture : public testing::Test
+{
+public:
 	SimilarityChecker similarityChecker;
+};
+TEST_F(SimilarityChecker_Fixture, PerfectMatch) {
 	EXPECT_EQ(60, similarityChecker.checkLengthSimilarity("ASD", "DSA"));
 }
-TEST(SimilarityCheckerTest, SecondParamTwoTimesLongerThanFirstParam) {
-	SimilarityChecker similarityChecker;
+TEST_F(SimilarityChecker_Fixture, SecondParamTwoTimesLongerThanFirstParam) {
 	EXPECT_EQ(0, similarityChecker.checkLengthSimilarity("A", "DS"));
 }
-TEST(SimilarityCheckerTest, FirstParamTwoTimesLongerThanSecondParam) {
-	SimilarityChecker similarityChecker;
+TEST_F(SimilarityChecker_Fixture, FirstParamTwoTimesLongerThanSecondParam) {
 	EXPECT_EQ(0, similarityChecker.checkLengthSimilarity("AA", "D"));
 }
-TEST(SimilarityCheckerTest, PartialPointWithLongFirstParam) {
-	SimilarityChecker similarityChecker;
+TEST_F(SimilarityChecker_Fixture, PartialPointWithLongFirstParam) {
 	EXPECT_EQ(40, similarityChecker.checkLengthSimilarity("AAABB", "BAA"));
 }
-TEST(SimilarityCheckerTest, PartialPointWithLongSecondParam) {
-	SimilarityChecker similarityChecker;
+TEST_F(SimilarityChecker_Fixture, PartialPointWithLongSecondParam) {
 	EXPECT_EQ(40, similarityChecker.checkLengthSimilarity("BAA", "AAABB"));
 }
